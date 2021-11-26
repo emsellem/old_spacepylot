be571cb7b9550a0cd373fa90afc12e3e2e68baca
Elizabeth Watkins <liz_j_watkins@hotmail.co.uk>
2021-11-26 16:44:27 +0100

    changed test to make it a better template

--- test.py
+++ test.py
@@ -5,29 +5,24 @@ Created on Thu Nov 11 21:03:10 2021
 @author: Liz_J
 """
 
-import autoAlignment.alignment as align
+import astroAlign.alignment as align
+import astroAlign.plotting as pl
 
-ngc = '1672'
 
-GALAXY = 'n' + ngc
-PATH = 'C:\\Users\\Liz_J\\Documents\\muse_new_align_test\\%s\\' % GALAXY
-
-ref_name = 'NGC%s_Rc_flux_nosky-matched_rpj_0001.fits'
-ref_path = PATH + '\\WFI_BB\\wfi_bb_ref\\P01\\' + ref_name % ngc
-
-pre_name = 'simplified_hdu_0001.fits'
-prealign_path = PATH + 'WFI_BB\\simplified_hdu\\P01\\' + pre_name
+ref_path = 
+prealign_path =
 trys = 10
 shifts_op = [0,0]
 rotation_op = 0
 
+convolve_prealign = 1
 
-op = align.Align_optical_flow.from_fits(prealign_path, ref_path, guess_translation=shifts_op, guess_rotation=rotation_op, convolve_prealign=None)
+op = align.Align_optical_flow.from_fits(prealign_path, ref_path, guess_translation=shifts_op, guess_rotation=rotation_op, convolve_prealign=convolve_prealign, verbose=True)
+shifts_op, rotation_op = op.get_translation_rotation(itera=1)
 
-shifts_op, rotation_op = op.get_translation_rotation()
+op_plot = pl.AlignmentPlotting.from_align_object(op)
 
-op_plot = AlignmentPlotting.from_align_object(op)
-plt.clf()
 op_plot.red_blue_before_after()
+op_plot.illistrate_vector_fields()
 
 
